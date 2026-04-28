c3ffa330cb37b5d13c3fc97f2eb2270b353a60ab
gitoshh <karani.gitonga.godwin@gmail.com>
2026-04-28 23:11:47 +0300

    add postgres db

--- database.py
+++ database.py
@@ -2,9 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.declarative import declarative_base
 from typing import Generator
+from dotenv import load_dotenv
+import os
 
-SQL_ALCHEMY_DATABASE_URI = 'sqlite:///todo.db'
-engine = create_engine(SQL_ALCHEMY_DATABASE_URI, connect_args={"check_same_thread": False})
+load_dotenv()
+DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
+# engine = create_engine(DATABASE_URI, connect_args={"check_same_thread": False}) -- for SQLite
+engine = create_engine(DATABASE_URI) # for PostgreSQL
 session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
